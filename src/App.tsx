7ddc9d1d6222f387ec4665e8067a08b431146e82
Ignacio <100495680@alumnos.uc3m.es>
2025-11-10 20:41:12 +0800

    fix: error

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import { VerifyEmail } from "./pages/VerifyEmail";
 import Pricing from "./pages/Pricing";
 import { Settings } from "./pages/Settings";
 import NotFound from "./pages/NotFound";
-import { AudioDemo } from "./components/AudioDemo";
 
 const queryClient = new QueryClient();
 
@@ -27,7 +26,6 @@ const App = () => (
             <Route path="/verify-email" element={<VerifyEmail />} />
             <Route path="/pricing" element={<Pricing />} />
             <Route path="/settings" element={<Settings />} />
-            <Route path="/audio-demo" element={<AudioDemo />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
